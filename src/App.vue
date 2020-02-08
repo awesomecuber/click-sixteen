9327300d65ff40a2f4bede5f39bacbc3d0b87a5a
Nicolas Brassard <awesomecuber10401@gmail.com>
2020-02-08 00:12:40 -0800

    made board size infinitely better

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,36 @@
-<template>
-  <div id="app">
-    <div
-      class="w-screen h-screen bg-blue-200 mx-auto flex flex-col items-center justify-around"
-    >
-      <div class="w-full">
-        <div
-          v-if="started"
-          class="text-5xl w-full flex flex-col sm:flex-row items-center sm:items-start justify-around"
-        >
-          <p>
-            Current: <b>{{ cur === 17 ? "DONE" : cur }}</b>
-          </p>
-          <p>
-            Time: <b>{{ (time / 100).toFixed(2) + "s" }}</b>
-          </p>
-        </div>
-        <div
-          v-else
-          class="text-5xl w-full flex flex-col sm:flex-row items-center sm:items-start justify-around"
-        >
-          <p>Click Sixteen</p>
-          <p>
-            Last Time:
-            <b>{{
-              lastTime === 0 ? "N/A" : (lastTime / 100).toFixed(2) + "s"
-            }}</b>
-          </p>
-        </div>
-      </div>
-      <div class="m-5 flex-1 w-11/12 md:w-3/4 border border-gray-700">
-        <div v-if="started" class="h-full flex flex-wrap items-stretch">
-          <div v-for="num in nums" :key="num" class="w-1/4 h-1/4">
-            <button
-              @click="increment(num)"
-              class="w-full h-full border-2 border-gray-700 bg-gray-300 text-6xl"
-            >
-              {{ num }}
-            </button>
-          </div>
-        </div>
-        <div v-else class="w-full h-full">
-          <button
-            @click="start"
+<template lang="pug">
+#app
+  #everything(class="w-screen h-screen bg-blue-200 mx-auto flex flex-col items-center justify-around")
+    #topbar(class="w-full")
+      #topbarstarted(
+        v-if="started"
+        class="text-5xl w-full flex flex-col sm:flex-row items-center sm:items-start justify-around"
+      )
+        p Current: <b>{{ cur }}</b>
+        p Time: <b>{{ (time / 100).toFixed(2) + "s" }}</b>
+      #topbarnotstarted(
+        v-else
+        class="text-5xl w-full flex flex-col sm:flex-row items-center sm:items-start justify-around"
+      )
+        p Click Sixteen
+        p Last Time: <b>{{ lastTime === 0 ? "N/A" : (lastTime / 100).toFixed(2) + "s" }}</b>
+
+    #playarea(class="m-5 flex-1 w-11/12 md:w-3/4 border border-gray-700")
+      #playareastarted(v-if="started" class="h-full flex flex-wrap items-stretch")
+        #numberbuttons(v-for="num in nums" :key="num" :class="buttonSize")
+          button(
+            @click="increment(num)"
+            class="w-full h-full border-2 border-gray-700 bg-gray-300 text-6xl"
+          ) {{ num }}
+      #playareanotstarted(v-else class="w-full h-full")
+        #optionsbuttons(class="flex flex-col w-full h-full")
+          button(
+            v-for="num in [3, 4, 5, 6]"
+            @click="start(num)"
             class="w-full h-full border border-gray-700 bg-gray-400"
-          >
-            <p class="text-6xl">
-              START
-            </p>
-            <p class="text-2xl text-gray-700">
-              Click all the numbers from 1 to 16
-            </p>
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
+          )
+            p(class="text-5xl font-bold") START {{ num * num }}
+            p(class="text-2xl text-gray-700") Click all the numbers from 1 to {{ num * num }}
 </template>
 
 <script>
@@ -63,6 +39,7 @@ export default {
   data: () => {
     return {
       nums: [],
+      size: 0,
       started: false,
       cur: 1,
       time: 0,
@@ -70,18 +47,23 @@ export default {
       lastTime: 0
     };
   },
-  created() {
-    const nums = [];
-    for (let i = 1; i <= 16; i++) {
-      nums.push(i);
+  computed: {
+    buttonSize: function() {
+      return `w-1/${this.size} h-1/${this.size}`;
     }
-    this.randomize(nums);
-    this.nums = nums;
   },
   methods: {
-    start() {
-      this.started = true;
+    start(num) {
+      let nums = [];
+      for (let i = 1; i < num * num + 1; i++) {
+        nums.push(i);
+      }
+      this.randomize(nums);
+      this.nums = nums;
 
+      this.size = num;
+
+      this.started = true;
       this.timer = setInterval(() => {
         this.time += 1;
       }, 10);
@@ -90,13 +72,12 @@ export default {
       if (num === this.cur) {
         this.cur += 1;
       }
-      if (this.cur === 17) {
+      if (this.cur > this.size * this.size) {
         clearTimeout(this.timer);
         this.started = false;
         this.lastTime = this.time;
         this.time = 0;
         this.cur = 1;
-        this.randomize(this.nums);
       }
     },
     randomize(arr) {
